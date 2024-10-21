772113065e773fe2a42504e782e80aba601dbdf5
Star-code1 <73273714+Star-code1@users.noreply.github.com>
2024-10-22 02:12:48 +0700

    Cap nhat trang thai hoat dong

---
+++ src/main/java/qlhtt/Controllers/NguoiQuanLy/CapNhatTrangThaiHoatDongController.java
@@ -0,0 +1,35 @@
+package qlhtt.Controllers.NguoiQuanLy;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CapNhatTrangThaiHoatDong {
+
+    @FXML
+    private TableView<?> bangNhanVien;
+
+    @FXML
+    private TableColumn<?, ?> gioiTinh;
+
+    @FXML
+    private TextField sdtTimKiem;
+
+    @FXML
+    private TableColumn<?, ?> soDienThoai;
+
+    @FXML
+    private TableColumn<?, ?> tenNhanVien;
+
+    @FXML
+    private JFXButton timBtn;
+
+    @FXML
+    private TableColumn<?, ?> trangThai;
+
+    @FXML
+    private TableColumn<?, ?> vaiTro;
+
+}
